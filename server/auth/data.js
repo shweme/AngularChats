a9dfef69383a960b8ba22e2973b5f9ef8f05e1db
Shweta Mehta <smehta2111@gmail.com>
2019-09-04 16:28:58 +1000

    Groups display in progress

--- server/auth/data.js
+++ server/auth/data.js
@@ -30,3 +30,31 @@ module.exports.getLogin = function getLogin(name, pwd){
     })
     
 }
+
+
+//not working yet
+
+/*
+module.exports.getGroups = function getGroups(name){
+    return new Promise((resolve, reject) => {
+        fs.readFile('auth/channel_members.json', 'utf8', (err, fileContents) => {
+            if(err){
+                reject(err);
+            } 
+            try{
+                let groups;
+                const data = JSON.parse(fileContents);
+                for (let i=0; i < data.length; i++){
+                    groups[i] = data[i].group;
+                    for(let j=0; j < data[i].length; j++){
+                        for(let k=0; k < data[i].members.length; k++){
+                            if(name == data[i].members[i]){
+                                groups[i]
+                            }
+                        }
+                    }
+                }
+            }
+    });
+}
+*/
